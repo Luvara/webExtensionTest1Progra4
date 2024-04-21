20636faeb55e6793529502cb5ba359a9cb0c8859
Luvara <luvarapz@gmail.com>
2024-04-21 12:57:56 -0600

    creacion de componentes
    como hasta el momento todo el código esta en el app.tsx, lo separare por componentes para mayor control y orden.
    el primero son los botones de elección del motor de busqueda

---
+++ webExtension/src/components/EngineSelectorComponent.tsx
@@ -0,0 +1,54 @@
+import React from "react";
+
+interface OptionSelectorProps {
+  selectedOption: string;
+  handleOptionChange: (option: string) => void;
+}
+
+const OptionSelector: React.FC<OptionSelectorProps> = ({ selectedOption, handleOptionChange }) => {
+  return (
+    <div className="flex">
+      <input
+        type="radio"
+        id="option1"
+        name="options"
+        value="option1"
+        className="sr-only"
+        checked={selectedOption === "option1"}
+        onChange={() => handleOptionChange("option1")}
+      />
+      <label
+        htmlFor="option1"
+        className={`px-4 py-2 rounded-lg w-1/2 mr-1 hover:cursor-pointer ${
+          selectedOption === "option1"
+            ? "bg-blue-500 text-black font-bold"
+            : "border-0 bg-gray-200 opacity-50"
+        }`}
+      >
+        GEMINI
+      </label>
+
+      <input
+        type="radio"
+        id="option2"
+        name="options"
+        value="option2"
+        className="sr-only"
+        checked={selectedOption === "option2"}
+        onChange={() => handleOptionChange("option2")}
+      />
+      <label
+        htmlFor="option2"
+        className={`px-4 py-2 rounded-lg w-1/2 ml-1 hover:cursor-pointer ${
+          selectedOption === "option2"
+            ? "bg-blue-500 text-black font-bold"
+            : "border-0 bg-gray-200 opacity-50"
+        }`}
+      >
+        ChatGPT
+      </label>
+    </div>
+  );
+};
+
+export default OptionSelector;
