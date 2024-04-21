1f78d54bbc1229366192b9a0cc7cabd5fe5e0df6
Luvara <luvarapz@gmail.com>
2024-04-21 13:40:21 -0600

    Create TextInputComponent.tsx
    component para el manejo del textInput.
    interface para con datos como el texto enviado y el onPrompt para el manejo de los cambios del text al escribir.
    Asi como un componente funcional de react para el manejo de los datos enviados por la interface.

---
+++ webExtension/src/components/TextInputComponent.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+interface TextInputProps {
+  prompt: string;
+  onPromptChange: (newPrompt: string) => void;
+}
+
+const TextInput: React.FC<TextInputProps> = ({ prompt, onPromptChange }) => {
+  const handleChange = (event: React.ChangeEvent<HTMLTextAreaElement>) => {
+    onPromptChange(event.target.value);
+  };
+
+  return (
+    <textarea
+      className="p-5 border-2 bg-slate-300 border-black rounded-lg text-sm"
+      id="texto"
+      name="texto"
+      rows={10}
+      cols={75}
+      required
+      value={prompt}
+      onChange={handleChange}
+    ></textarea>
+  );
+};
+
+export default TextInput;
