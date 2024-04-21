c55cafeec9d63cc55623c4fd20f1a93a21b1dd67
Luvara <luvarapz@gmail.com>
2024-04-21 13:25:48 -0600

    Create FlowSelectorComponent.tsx
    creación el componente de selección de los flujos de trabajo.
    uso de interface para definir las propiedades que se pueden pasar entre componentes

---
+++ webExtension/src/components/FlowSelectorComponent.tsx
@@ -0,0 +1,35 @@
+import React from "react";
+
+interface FlowSelectorProps {
+  selectedFlow: string;
+  handleFlowChange: (flow: string) => void;
+}
+
+const FlowSelector: React.FC<FlowSelectorProps> = ({ selectedFlow, handleFlowChange }) => {
+  return (
+    <div>
+      <label className="mr-1">
+        <input
+          className="mr-1"
+          type="radio"
+          value="flujo1"
+          checked={selectedFlow === "flujo1"}
+          onChange={() => handleFlowChange("flujo1")}
+        />
+        Flujo Reflexivo
+      </label>
+      <label className="ml-1">
+        <input
+          className="mr-1"
+          type="radio"
+          value="flujo2"
+          checked={selectedFlow === "flujo2"}
+          onChange={() => handleFlowChange("flujo2")}
+        />
+        Flujo Multiagente
+      </label>
+    </div>
+  );
+};
+
+export default FlowSelector;
